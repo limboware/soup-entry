b09ccd664043b25ae9a1f001f394818cdfdf8693
rejchev <hendlerq@yandex.ru>
2026-09-03 14:39:11 +0500

    ref (logger): wrap plugify log

--- pkg/logger/v1/logger.go
+++ pkg/logger/v1/logger.go
@@ -1,14 +1,14 @@
 package loggerv1
 
 import (
-	"log"
+	"fmt"
 
 	configsv2 "github.com/limboware/pkg/configs/v2"
 	errnov1 "github.com/rejchev/errno"
+	"github.com/untrustedmodders/go-plugify"
 )
 
 type Logger struct {
-	base  *log.Logger
 	flags LoggerFlag
 }
 
@@ -19,7 +19,6 @@ func Get() *Logger {
 }
 
 func (x *Logger) Init() errnov1.Code {
-	x.base = log.Default()
 	x.flags = None
 
 	if !Parse(configsv2.Get().ValueA("logs", "sys,errs,warns"), &x.flags) {
@@ -29,36 +28,54 @@ func (x *Logger) Init() errnov1.Code {
 	return errnov1.OK
 }
 
-func (x *Logger) Closer() *log.Logger {
-	return x.base
+func (x *Logger) severity(v LoggerFlag) plugify.Severity {
+	switch v {
+	case PrintLogs:
+		return plugify.Info
+
+	case PrintDebug:
+		return plugify.Debug
+
+	case PrintErrs:
+		return plugify.Error
+
+	case PrintWarns:
+		return plugify.Warning
+
+	case PrintSystem:
+		return plugify.Info
+
+	default:
+		return plugify.Unknown
+	}
 }
 
 func (x *Logger) Log(buff string, v ...any) {
-	if x.base != nil && (x.flags&PrintLogs) != 0 {
-		x.Closer().Printf(buff+"\n", v...)
+	if (x.flags & PrintLogs) != 0 {
+		plugify.Log(fmt.Sprintf(buff, v...), x.severity(PrintLogs), "", 0)
 	}
 }
 
 func (x *Logger) Debug(buff string, v ...any) {
-	if x.base != nil && (x.flags&PrintDebug) != 0 {
-		x.Closer().Printf(buff+"\n", v...)
+	if (x.flags & PrintDebug) != 0 {
+		plugify.Log(fmt.Sprintf(buff, v...), x.severity(PrintDebug), "", 0)
 	}
 }
 
 func (x *Logger) System(buff string, v ...any) {
-	if x.base != nil && (x.flags&PrintSystem) != 0 {
-		x.Closer().Printf(buff+"\n", v...)
+	if (x.flags & PrintSystem) != 0 {
+		plugify.Log(fmt.Sprintf(buff, v...), x.severity(PrintSystem), "", 0)
 	}
 }
 
 func (x *Logger) Warn(buff string, v ...any) {
-	if x.base != nil && (x.flags&PrintWarns) != 0 {
-		x.Closer().Printf(buff+"\n", v...)
+	if (x.flags & PrintWarns) != 0 {
+		plugify.Log(fmt.Sprintf(buff, v...), x.severity(PrintWarns), "", 0)
 	}
 }
 
 func (x *Logger) Err(buff string, v ...any) {
-	if x.base != nil && (x.flags&PrintErrs) != 0 {
-		x.Closer().Printf(buff+"\n", v...)
+	if (x.flags & PrintErrs) != 0 {
+		plugify.Log(fmt.Sprintf(buff, v...), x.severity(PrintErrs), "", 0)
 	}
 }
