f59e537c00face6cb19eae3021f8fa95594ef053
rejchev <hendlerq@yandex.ru>
2026-09-04 18:49:55 +0500

    ref: bind client conn/discon with soupe logic

--- soupe/main.go
+++ soupe/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
+	limbov1 "github.com/limboware/limbo"
 	soupev1 "github.com/limboware/soupe/internal/v1"
 	errnov1 "github.com/rejchev/errno"
 	"github.com/untrustedmodders/go-plugify"
+	s2sdk "github.com/untrustedmodders/s2sdk/213"
 )
 
 func onPluginStart() error {
@@ -13,7 +15,23 @@ func onPluginStart() error {
 		return fmt.Errorf("soupe err on start up (err: %s)", err.String())
 	}
 
+	s2sdk.OnClientAuthenticated_Register(onClientAuthenticated)
+	s2sdk.OnClientDisconnect_Register(onClientDisconnect)
+
 	return nil
+}
+
+func onClientAuthenticated(playerSlot int32, steamID uint64) {
+	limbov1.Events().PublishAsync("clients.connected", &soupev1.ClientConnectedEvent{
+		Slot:    playerSlot,
+		SteamID: steamID,
+	})
+}
+
+func onClientDisconnect(playerSlot int32) {
+	limbov1.Events().PublishAsync("clients.disconnect", &soupev1.ClientConnectedEvent{
+		Slot:    playerSlot,
+	})
 }
 
 func onPluginUpdate(dt float32) error {
@@ -22,6 +40,8 @@ func onPluginUpdate(dt float32) error {
 }
 
 func onPluginEnd() error {
+	s2sdk.OnClientAuthenticated_Unregister(onClientAuthenticated)
+	s2sdk.OnClientDisconnect_Unregister(onClientDisconnect)
 	soupev1.Get().Destroy()
 	return nil
 }
